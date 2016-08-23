912385aafc9ba4de7f86a1d0752ea670e4f08662
Takashi Oguma <oguma@soracom.jp>
2016-08-23 09:52:15 +0900

    add suspend_subscriber

---
+++ soracom/generated/cmd/subscribers_suspend.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var SubscribersSuspendCmdImsi string
+
+func init() {
+	SubscribersSuspendCmd.Flags().StringVar(&SubscribersSuspendCmdImsi, "imsi", "", TR("subscribers.suspend_subscriber.post.parameters.imsi.description"))
+
+	SubscribersCmd.AddCommand(SubscribersSuspendCmd)
+}
+
+var SubscribersSuspendCmd = &cobra.Command{
+	Use:   "suspend",
+	Short: TR("subscribers.suspend_subscriber.post.summary"),
+	Long:  TR(`subscribers.suspend_subscriber.post.description`),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		opt := &apiClientOptions{
+			Endpoint: getSpecifiedEndpoint(),
+			BasePath: "/v1",
+			Language: getSelectedLanguage(),
+		}
+
+		ac := newAPIClient(opt)
+		if v := os.Getenv("SORACOM_VERBOSE"); v != "" {
+			ac.SetVerbose(true)
+		}
+
+		err := authHelper(ac, cmd, args)
+		if err != nil {
+			cmd.SilenceUsage = true
+			return err
+		}
+
+		param, err := collectSubscribersSuspendCmdParams()
+		if err != nil {
+			return err
+		}
+
+		result, err := ac.callAPI(param)
+		if err != nil {
+			cmd.SilenceUsage = true
+			return err
+		}
+
+		if result != "" {
+			return prettyPrintStringAsJSON(result)
+		} else {
+			return nil
+		}
+	},
+}
+
+func collectSubscribersSuspendCmdParams() (*apiParams, error) {
+
+	return &apiParams{
+		method: "POST",
+		path:   buildPathForSubscribersSuspendCmd("/subscribers/{imsi}/suspend"),
+		query:  buildQueryForSubscribersSuspendCmd(),
+	}, nil
+}
+
+func buildPathForSubscribersSuspendCmd(path string) string {
+
+	path = strings.Replace(path, "{"+"imsi"+"}", SubscribersSuspendCmdImsi, -1)
+
+	return path
+}
+
+func buildQueryForSubscribersSuspendCmd() string {
+	result := []string{}
+
+	return strings.Join(result, "&")
+}
