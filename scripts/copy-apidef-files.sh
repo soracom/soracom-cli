98458526883200e7bce3526687b64090d7d3250f
Takashi Oguma <oguma@soracom.jp>
2019-03-18 10:31:46 +0900

    update source directory

--- scripts/copy-apidef-files.sh
+++ scripts/copy-apidef-files.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 d="$( cd "$( dirname "$0" )" || exit 1; cd ..; pwd )"
 
-cp "$d/../soracom-api/apidef/prod/soracom-api.en.yaml" "$d/generators/assets/"
-cp "$d/../soracom-api/apidef/prod/i18n/soracom-api.ja.yaml" "$d/generators/assets/"
+fen="$d/../soracom-api/build/soracom-api.en.yaml"
+fja="$d/../soracom-api/build/soracom-api.ja.yaml"
+
+if [ ! -f "$fen" ] || [ ! -f "$fja" ]; then
+  echo "API definition files not found. Build soracom-api first."
+  exit 1
+fi
+
+cp "$d/../soracom-api/build/soracom-api.en.yaml" "$d/generators/assets/"
+cp "$d/../soracom-api/build/soracom-api.ja.yaml" "$d/generators/assets/"
 
