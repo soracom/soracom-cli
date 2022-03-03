94a8e42670ff571390a461ea26dcc07f197a4cf8
Takashi Oguma <oguma@soracom.jp>
2022-03-03 18:21:31 +0900

    add an option to get quieter

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ extract() {
       tar -C "$dir" -xf "$path"
       ;;
     ".zip")
-      unzip "$path" -d "$dir"
+      unzip -q "$path" -d "$dir"
       ;;
     *)
       echo "unknown archive extension: $ext"
