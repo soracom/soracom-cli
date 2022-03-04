96b257b0240d78d79698e539919508014aeff52f
Takashi Oguma <oguma@soracom.jp>
2022-03-04 14:36:42 +0900

    create snap packages for arm64 and armhf

--- snap/build.sh
+++ snap/build.sh
@@ -1,7 +1,52 @@
 #!/usr/bin/env bash
-set -e
+set -Eeuo pipefail
 set -x
+
+VERSION=$1
 
 cd /mnt
 ls
-snapcraft 
+
+generate_snapcraft_yaml() {
+  local arch=$1
+  goarch=$arch
+  if [ "$goarch" == "armhf" ]; then
+    goarch="arm"
+  fi
+
+  mkdir -p "/mnt/snap"
+  cat <<EOD > "/mnt/snap/snapcraft.yaml"
+name: soracom
+version: '$VERSION'
+summary: A CLI tool to use SORACOM API.
+description: |
+  This is a tool to invoke SORACOM API. You can control SORACOM platform
+  and its services automatically by using this tool.
+
+grade: stable
+confinement: strict
+architectures:
+  - build-on: all
+    run-on: ${arch}
+
+parts:
+  soracom:
+    plugin: dump
+    source: soracom_${VERSION}_linux_${goarch}.tar.gz
+    override-build: |
+      chmod +x soracom
+      snapcraftctl build
+
+apps:
+  soracom:
+    command: soracom
+EOD
+}
+
+for arch in amd64 arm64 armhf; do
+  generate_snapcraft_yaml "$arch"
+  snapcraft clean soracom -s pull
+  snapcraft
+done
+
+snapcraft clean
--- snap/release.sh
+++ snap/release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+set -Eeuo pipefail
+set -x
 
 VERSION=$1
 
 snapcraft login
 
-set -e
-set -x
-
-res="$( snapcraft upload "/mnt/soracom_${VERSION}_amd64.snap" )"
-rev="$( echo "$res" | sed -rn 's/.*Revision ([0-9]+) of .* created./\1/p' )"
-channels=beta,edge,candidate,stable
-snapcraft release soracom "$rev" "$channels"
+for arch in amd64 arm64 armhf; do
+  res="$( snapcraft upload "/mnt/soracom_${VERSION}_${arch}.snap" )"
+  rev="$( echo "$res" | sed -rn 's/.*Revision ([0-9]+) of .* created./\1/p' )"
+  channels=beta,edge,candidate,stable
+  snapcraft release soracom "$rev" "$channels"
+done
