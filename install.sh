38bf81c5c97d4364ee115e1524b469d91072ab8c
Takashi Oguma <oguma@soracom.jp>
2022-03-08 09:14:36 +0900

    show progress bar while downloading

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ goarch="$( get_goarch )"
 ext_regexp="$( get_ext_regexp "$goos" )"
 ext="$( get_ext "$goos" )"
 
-url="$( \curl -fsSL https://api.github.com/repos/soracom/soracom-cli/releases/latest | \
+url="$( \curl -fSL# https://api.github.com/repos/soracom/soracom-cli/releases/latest | \
   \grep 'browser_download_url' | \
   \grep "${goos}_${goarch}${ext_regexp}" | \
   cut -d : -f 2-3 | \
